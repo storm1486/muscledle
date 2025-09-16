55a5659faece45a230446fd8a4a7e7588d429083
patemit <patelmit8292@gmail.com>
2025-09-16 11:19:20 -0400

    testing if models load faster

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/models/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/models/manifest.json",
+        headers: [
+          { key: "Cache-Control", value: "public, max-age=3600" }, // or longer if stable
+        ],
+      },
+    ];
+  },
 };
-
-export default nextConfig;
